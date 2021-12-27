ac7d3a89964a14f7e7e5cc3846a0120d7f792270
Tanishq Chamola <chamola.tanishq@gmail.com>
2021-12-28 02:24:45 +0530

    json-server added and setup

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,21 @@
 import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./shared/Spinner";
 
 import FeedbackContext from "../context/FeedbackContext";
 
 import { motion, AnimatePresence } from "framer-motion";
 
 function FeedbackList() {
-	const { feedback } = useContext(FeedbackContext);
+	const { feedback, isLoading } = useContext(FeedbackContext);
 
-	if (!feedback || feedback.length === 0) {
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <p>No Feedback yet!</p>;
 	}
 
-	return (
+	return isLoading ? (
+		<Spinner />
+	) : (
 		<div className="feedback-list">
 			<AnimatePresence>
 				{feedback.map((item) => {
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,44 +1,54 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { uid } from "../data/UniqueID";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: "kx3pzgwi0mwgkf5zz1ip",
-			rating: 10,
-			text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-		},
-		{
-			id: "kx3q03ppxrga6uzwnt",
-			rating: 9,
-			text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-		},
-		{
-			id: "kx3q0d5rhz7op4jnvg9",
-			rating: 8,
-			text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-		},
-	]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [feedback, setFeedback] = useState([]);
 
 	const [feedbackEditItem, setFeedbackEditItem] = useState({
 		item: {},
 		edit: false,
 	});
 
+	// if dependency array is empty it will run only once
+	useEffect(() => {
+		fetchFeedback();
+	}, []);
+
+	// fetch feedback data
+	const fetchFeedback = async () => {
+		const response = await fetch("/feedback?_sort=id&_order=desc");
+		const data = await response.json();
+
+		setFeedback(data);
+		setIsLoading(false);
+	};
+
 	// adds feedback
-	const addFeedback = (newFeedback) => {
+	const addFeedback = async (newFeedback) => {
 		if (newFeedback) {
+			const response = await fetch("/feedback", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newFeedback),
+			});
+
+			const data = await response.json();
 			newFeedback.id = uid();
-			console.log(newFeedback);
-			setFeedback([newFeedback, ...feedback]);
+			setFeedback([data, ...feedback]);
 		}
 	};
 
 	// deletes feedback
-	const deleteFeedback = (id) => {
+	const deleteFeedback = async (id) => {
 		if (window.confirm("Are you sure yo want to delete?")) {
+			await fetch(`/feedback/${id}`, {
+				method: "DELETE",
+			});
 			const feedbackAfterDelete = feedback.filter((item) => {
 				return item.id !== id;
 			});
@@ -55,11 +65,20 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// update feedback item
-	const updateFeedback = (id, updatedItem) => {
-		setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updatedItem } : item)));
+	const updateFeedback = async (id, updatedItem) => {
+		const response = await fetch(`/feedback/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedItem),
+		});
+		const data = await response.json();
+
+		setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...data } : item)));
 	};
 
-	return <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback, editFeedback, updateFeedback, feedbackEditItem }}>{children}</FeedbackContext.Provider>;
+	return <FeedbackContext.Provider value={{ feedback, feedbackEditItem, isLoading, deleteFeedback, addFeedback, editFeedback, updateFeedback }}>{children}</FeedbackContext.Provider>;
 };
 
 export default FeedbackContext;
